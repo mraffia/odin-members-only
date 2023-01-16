7c17bd93ddedfa9966d1e7384c0706de28eac023
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-17 02:14:26 +0700

    Fill the index route with router that will call the controller functions

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// Require controller modules.
+const user_controller = require("../controllers/userController");
+const message_controller = require("../controllers/messageController");
+
+/// USER ROUTES ///
+
+router.get("/", user_controller.index);
+
+router.get("/signup", user_controller.user_signup_get);
+
+router.post("/signup", user_controller.user_signup_post);
+
+router.get("/login", user_controller.user_login_get);
+
+router.post("/login", user_controller.user_login_post);
+
+router.get("/logout", user_controller.user_logout_get);
+
+router.get("/join-the-club", user_controller.user_join_the_club_get);
+
+router.post("/join-the-club", user_controller.user_join_the_club_post);
+
+/// MESSAGE ROUTES ///
+
+router.get("/message-create", message_controller.message_create_get);
+
+router.post("/message-create", message_controller.message_create_post);
+
+router.get("/message-delete/:id", message_controller.message_delete_get);
+
+router.post("/message-delete/:id", message_controller.message_delete_post);
 
 module.exports = router;
