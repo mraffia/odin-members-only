2b24914d46e8b0b8ed5674e88cfa51dd0215afb1
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-17 03:44:49 +0700

    Add logic to the user_signup_get, user_signup_post, and index controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,36 @@
 const User = require("../models/user");
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
 
 exports.index = (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Site Home Page");
+    res.render("index", { title: "Members Only" });
 };
 
 exports.user_signup_get = (req, res, next) => {
-    res.send("NOT IMPLEMENTED");
+    res.render("signup_form", { title: "Sign Up" });
 };
 
 exports.user_signup_post = (req, res, next) => {
-    res.send("NOT IMPLEMENTED");
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        if (err) {
+            return next(err);
+        } else {
+            const is_admin_status = req.body.is_admin ? true : false;
+
+            const user = new User({
+                name: req.body.name,
+                username: req.body.username,
+                password: hashedPassword,
+                is_member: false,
+                is_admin: is_admin_status,
+            }).save(err => {
+                if (err) { 
+                    return next(err);
+                }
+                res.redirect("/");
+            });
+        }
+    });
 };
 
 exports.user_login_get = (req, res, next) => {
