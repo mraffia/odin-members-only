c0cc36bb54e7adaf8f2f79c63b21adcd0ea86113
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-17 02:15:04 +0700

    Create message controller skeleton

---
+++ controllers/messageController.js
@@ -0,0 +1,17 @@
+const Message = require("../models/message");
+
+exports.message_create_get = (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+};
+
+exports.message_create_post = (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+};
+
+exports.message_delete_get = (req, res, next) => {
+    res.send(`NOT IMPLEMENTED`);
+};
+
+exports.message_delete_post = (req, res, next) => {
+    res.send(`NOT IMPLEMENTED`);
+};
