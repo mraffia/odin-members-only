abdb4f3524da2ea6c349a2cbe07425416ffff658
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-17 02:14:52 +0700

    Create user controller skeleton

---
+++ controllers/userController.js
@@ -0,0 +1,33 @@
+const User = require("../models/user");
+
+exports.index = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Site Home Page");
+};
+
+exports.user_signup_get = (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+};
+
+exports.user_signup_post = (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+};
+
+exports.user_login_get = (req, res, next) => {
+    res.send(`NOT IMPLEMENTED`);
+};
+
+exports.user_login_post = (req, res, next) => {
+    res.send(`NOT IMPLEMENTED`);
+};
+
+exports.user_logout_get = (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+};
+
+exports.user_join_the_club_get = (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+};
+
+exports.user_join_the_club_post = (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+};
