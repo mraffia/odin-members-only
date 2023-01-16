42f1fec9c33b3d4bc5a1df4f47251e6735149480
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-17 01:26:25 +0700

    Create user and message model and install async module

---
+++ models/message.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
+  message_text: { type: String, required: true },
+});
+
+// Virtual for message's URL
+MessageSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/message/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Message", MessageSchema);
---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  is_member: { type: Boolean, required: true, default: false },
+  is_admin: { type: Boolean, required: true, default: false },
+});
+
+// Virtual for user's URL
+UserSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/user/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("User", UserSchema);
